d4ccb4128b8af439d54ac9f2111cd4f89529bd4f
Austin McCallister <austinmccallister0@gmail.com>
2023-11-13 08:14:58 -0500

    getSAMLCookies case
    Sometimes the website throws an error when getting cookies, so I made a case where that happens and it waits a few seconds before trying again

--- main.py
+++ main.py
@@ -40,8 +40,10 @@ params = {
 def getSAMLCookies(sess: requests.Session):
     response = sess.get('https://pasco.focusschoolsoftware.com/focus/Modules.php?modname=misc%2FPortal.php', params=params, headers=headers)
     match = re.findall(r'value=\"(.+?)\"', response.text)
-    if (not match):
-        print(response.text)
+    if (not match): # Sometimes when refreshing token it throws an error getting cookies for some reason
+        time.sleep(5)
+        print("There was a problem getting SAML Cookies... Retrying\nContent: %s" % response.text)
+        return getSAMLCookies(sess)
     data = {
         'SAMLRequest': match[0],
         'RelayState': match[1]
